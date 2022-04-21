e8fd29c35537545fb435c0dbd635b4296b07092b
David Dodlek <david@dodlek.com>
2022-04-21 14:34:23 +0200

    style: reformatted with google-java-format

--- src/main/java/org/vaadin/addons/f0rce/uploadhelper/UHReceiver.java
+++ src/main/java/org/vaadin/addons/f0rce/uploadhelper/UHReceiver.java
@@ -6,13 +6,12 @@ import java.io.Serializable;
 @FunctionalInterface
 public interface UHReceiver extends Serializable {
 
-	/**
-	 * Invoked when a new upload arrives.
-	 *
-	 * @param fileName the desired filename of the upload, usually as specified by
-	 *                 the client
-	 * @param mimeType the MIME type of the uploaded file
-	 * @return stream to which the uploaded file should be written
-	 */
-	OutputStream receiveUpload(String fileName, String mimeType);
+  /**
+   * Invoked when a new upload arrives.
+   *
+   * @param fileName the desired filename of the upload, usually as specified by the client
+   * @param mimeType the MIME type of the uploaded file
+   * @return stream to which the uploaded file should be written
+   */
+  OutputStream receiveUpload(String fileName, String mimeType);
 }
